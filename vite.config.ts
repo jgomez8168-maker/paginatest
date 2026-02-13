bda24cd2057d51e0a5ec41dd26a6685f25ccfc17
Julian Andres Gomez Salazar <ja.gomez1@hv.com.co>
2026-02-12 22:14:35 -0500

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: "/paginatest", // BASE DE GITHUB O NOMBRE DEL REPO
+    base: "/paginatest/", // BASE DE GITHUB O NOMBRE DEL REPO
     server: {
       port: 3000,
       host: '0.0.0.0',
